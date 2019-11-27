da8e70a389b4f4f83bc55d28a875907a1ed7dd8d
SINGER Louis <louis.singer@efrei.net>
2019-11-27 15:48:49 +0100

    chat structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,3 +3,9 @@
     <router-view></router-view>
   </v-app>
 </template>
+
+<script>
+export default {
+  
+}
+</script>
--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -1,5 +1,17 @@
 <template>
     <v-container fluid>
+      <v-container md6>
+        <v-container md3>
+          // listOfPlayers
+        </v-container>
+        <v-container md9>
+          // Messages
+
+        </v-container>
+      </v-container>
+      <v-container md6>
+        
+      </v-container>
     </v-container>
 </template>
 
@@ -8,7 +20,11 @@ import { Component, Vue, Prop, Watch } from "vue-property-decorator";
 
 @Component
 export default class Chat extends Vue {
-
+  data() {
+    return {
+      players: [],
+    }
+  }
 }
 </script>
 
